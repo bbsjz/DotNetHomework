e94d62611af9e47732c930513ea8325fa900de51
bbg <2503668406@qq.com>
2022-02-25 13:40:22 +0800

    homework1 first commit

---
+++ aSimpleCalculator/aSimpleCalculator/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aSimpleCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string[] choice = new[] { "+", "-", "*", "/" };
+            comboBox1.Items.AddRange(choice);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("=");
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private int ifReasonable(ref double num,TextBox t,int count,int already)
+        {
+            try
+            {
+                num = double.Parse(t.Text);
+                return already;
+            }
+            catch
+            {
+                if(already!=0)
+                    label3.Text += "\n";
+                label3.Text += "第" + count + "个操作数非数字，请输入合法值";
+                already++;
+                return already;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int already = 0;
+            label3.Text = "";
+            double num1=0;
+            double num2=0;
+            already = ifReasonable(ref num1, textBox1, 1, already);
+            already = ifReasonable(ref num2, textBox2, 2, already);
+            string numOperator = comboBox1.Text;
+            if (already==0)
+            {
+                label3.Text = "";
+                switch (numOperator)
+                {
+                    case "+":
+                        label1.Text = Convert.ToString(num1 + num2);
+                        break;
+                    case "-":
+                        label1.Text = Convert.ToString(num1 - num2);
+                        break;
+                    case "*":
+                        label1.Text = Convert.ToString(num1 * num2);
+                        break;
+                    case "/":
+                        label1.Text = Convert.ToString(num1 / num2);
+                        break;
+                    default:
+                        label3.Text = "请选择运算符";
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ aSimpleCalculator/calculatorConsole/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace calculatorConsole
+{
+    class Program
+    {
+        private static double TryParse()
+        {
+            try
+            {
+                double num1 = double.Parse(Console.ReadLine());
+                return num1;
+            }
+            catch
+            {
+                Console.WriteLine("请按正确的格式输入参数！");
+                return 0;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("输入两个数字和一个运算符（+，-，*，/），每次输入一个值后请按回车，示例：2回车，3回车，+回车。此程序会打印出计算结果");
+            double num1 = TryParse();
+            double num2 = TryParse();
+            string numOperator = Console.ReadLine();
+            switch (numOperator)
+            {
+                case "+":
+                    Console.WriteLine(num1 + num2);
+                    break;
+                case "-":
+                    Console.WriteLine(num1 - num2);
+                    break;
+                case "*":
+                    Console.WriteLine(num1 * num2);
+                    break;
+                case "/":
+                    Console.WriteLine(num1 / num2);
+                    break;
+                default:
+                    Console.WriteLine("请输入合法的运算符（+，-，*，/）");
+                    break;
+            }
+        }
+    }
+}
