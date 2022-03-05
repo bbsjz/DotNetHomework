ae704c4678fc5671e0a07ac694436e1d12294114
bbg <2503668406@qq.com>
2022-03-05 12:20:17 +0800

    homework3

---
+++ homework3/homework3/Factor.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using problem1;
+
+namespace problem1
+{
+    class Factor
+    {
+        //制作工厂，其中num是选择码，[begin,end)是随机生成边的范围
+        public static Shape produce(int num,int begin,int end)
+        {
+            Random rd = new Random();
+            double rd1 = rd.Next(begin, end);
+            double rd2 = rd.Next(begin, end);
+            double rd3 = rd.Next(begin, end);
+            switch (num)
+            {
+                case 0://生成长方形             
+                    return new Rectangle(rd1, rd2);
+                case 1://生成正方形
+                    return new Squre(rd3);
+                case 2://生成三角形
+                    return new Triangle(rd1, rd2, rd3);
+                case 3://生成圆形
+                    return new Round(rd1);
+                default:
+                    throw new Exception("没有此形状");
+            }
+        }
+    }
+
+    class SimpleFactor
+    {
+        //num为要生成的随机形状个数
+        public static void Main()
+        {
+            ArrayList list = new ArrayList();
+            Random rd = new Random();
+            //生产十次，并检查产品是否合格，若有残次品则丢弃
+            for(int i=0;i<10;i++)
+            {
+                int r = rd.Next(0, 4);
+                Shape newRandomShape = Factor.produce(r, 0, 100);
+                if(newRandomShape.ifReasonable())
+                {
+                    list.Add(newRandomShape);
+                }
+            }
+            Console.WriteLine(Adder.sumArea(list));
+        }
+    }
+
+    class Adder
+    {
+        public static double sumArea(ArrayList list)
+        {
+            double sum = 0;
+            foreach(Object obj in list)
+            {
+                sum +=((Shape)obj).getArea();
+            }
+            return sum;
+        }
+    }
+}
---
+++ homework3/homework3/Shape.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace problem1
+{
+    abstract class Shape
+    {
+        public abstract double getArea();
+        public abstract bool ifReasonable();
+    }
+
+    //方形
+    class Rectangle : Shape
+    {
+        //属性
+        public double length{ get; }
+        public double width{ get; }
+        
+        //构造函数
+        public Rectangle(double l,double w)
+        {
+            length = l;
+            width = w;
+        }
+        
+        //方法
+        public override double getArea()
+        {
+            if (ifReasonable())
+                return length * width;
+            else
+                throw new Exception("长宽小于或等于0，此方形不合法");
+        }
+        public override bool ifReasonable()
+        {
+            return length > 0 && width > 0;
+        }
+    }
+
+    //正方形
+    class Squre : Rectangle
+    {
+        //构造函数
+        public Squre(double l) : base(l, l)
+        {
+
+        }
+    }
+
+    //三角形
+    class Triangle : Shape
+    {
+        //属性
+        public double side1 { get;  }
+        public double side2 { get; }
+        public double side3 { get; }
+        
+        //构造函数
+        public Triangle(double l1,double l2,double l3)
+        {
+            side1 = l1;
+            side2 = l2;
+            side3 = l3;
+        }
+        
+        //方法
+        public override double getArea()
+        {
+            if (ifReasonable())
+            {
+                double p = (side1 + side2 + side3) / 2;
+                return (Math.Pow(p * (p - side1) * (p - side2) * (p - side3), 0.5));
+            }
+            else
+                throw new Exception("输入边长小于或等于0，或三边无法构成三角形，此三角形不合法");
+            
+        }
+
+        public override bool ifReasonable()
+        {
+            return (side1 > 0 && side2 > 0 && side3 > 0)&&(side1+side2>side3)&&(side3+side1>side2)&&(side2+side3>side1);
+        }
+    }
+
+    //圆形
+    class Round : Shape
+    {
+        //属性
+        public double r { get; }
+        
+        //构造函数
+        public Round(double r)
+        {
+            this.r = r;
+        }
+
+        //方法
+        public override double getArea()
+        {
+            if (ifReasonable())
+                return Math.PI * r * r;
+            else
+                throw new Exception("半径小于或等于0，此圆形不合法");
+        }
+
+        public override bool ifReasonable()
+        {
+            return r > 0;
+        }
+    }
+}
